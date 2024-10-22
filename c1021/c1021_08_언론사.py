f4717889a606fa18f98ac77f2a254f22a03df66d
park <parkmk0615@naver.com>
2024-10-22 09:19:59 +0900

    웹스크래핑

--- c1021/c1021_08_언론사.py
+++ c1021/c1021_08_언론사.py
@@ -1,37 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-url="https://news.naver.com/main/ranking/popularDay.naver?mid=etc&sid1=111"
-headers={"User-Agent":" "}
-res=requests.get(url,headers=headers)
+url = "https://news.naver.com/main/ranking/popularDay.naver"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"}
+res = requests.get(url,headers=headers)
 res.raise_for_status()
 
-# html 전체를 가져옴
-soup=BeautifulSoup(res.text,"lxml")
+# html 전체를 가져옴.
+soup = BeautifulSoup(res.text,"lxml")
 cont = soup.find("div",{"class":"rankingnews_box_wrap _popularRanking"})
-ranklists = cont.find_all("div",{"class":"rankingnews_box"})
-print("갯수:",len(ranklists))
+rankLists = cont.find_all("div",{"class":"rankingnews_box"})
 
 
-# 파일 1줄씩 저장하시오
-# 파일 저장
-# f=open("a.html","w",encoding="utf-8")
-# f.write(res.text)
-# f.close()
-with open("c1021/news.txt","w",encoding="utf-8")as f:
-  print("갯수확인 : ", len(ranklists))
-  f.write(f"갯수확인: {len(ranklists)}\n")
-
-  for idx,ranklist in enumerate(ranklists):
+# 파일 1줄씩 저장하시오.
+with open("a.txt","w",encoding="utf-8") as f:
+  print("개수확인 : ",len(rankLists))
+  f.write(f"개수확인 : {len(rankLists)}\n")
+  for idx,rankList in enumerate(rankLists):
     # 언론사 이름 출력
-    print(f"[ {idx+1}. 언론사: {ranklist.find("strong",{"class":"rankingnews_name"}).text} ]")
-    f.write(f"[ {idx+1}. 언론사: {ranklist.find("strong",{"class":"rankingnews_name"}).text} ]\n")
-    news = ranklist.find("ul",{"class":"rankingnews_list"})
-    newslists = news.find_all("li")
-  # print("랭킹 박스 안 뉴스 갯수 : ",len(newslists))
+    print(f"[ {idx+1}. 언론사 : {rankList.find("strong",{"class":"rankingnews_name"}).text} ]")
+    f.write(f"[ {idx+1}. 언론사 : {rankList.find("strong",{"class":"rankingnews_name"}).text} ]\n")
+    news = rankList.find("ul",{"class":"rankingnews_list"})
+    newsLists = news.find_all("li")
+    # print("랭킹박스 안 뉴스 개수 : ",len(newsLists))
+    for i,newsList in enumerate(newsLists):
+      print(f"{i+1} : {newsList.find("a").text}")
+      f.write(f"{i+1} : {newsList.find("a").text}\n")
 
-    for i,newslists in enumerate(newslists):
-      print(f"{i+1}: {newslists.find("a").text}")
-      f.write(f"{i+1}: {newslists.find("a").text}\n")
-
-print("완료")
+print("완료")      
--- c1021/c1021_09_11번가.py
+++ c1021/c1021_09_11번가.py
@@ -1,25 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-url="http://www.11st.co.kr/browsing/BestSeller.tmall?method=getBestSellerMain&xfrom=main^gnb"
-headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"}
-res=requests.get(url,headers=headers)
+url = "https://www.11st.co.kr/browsing/BestSeller.tmall?method=getBestSellerMain&xfrom=main^gnb"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"}
+res = requests.get(url,headers=headers)
 res.raise_for_status()
 
-# soup 변환
-soup=BeautifulSoup(res.text,"lxml")
+# soup변환
+soup = BeautifulSoup(res.text,"lxml")
+# print(soup.find("div",{"id":"bestPrdList"}).h3.text)
 
-
-print(soup.find("div",{"id":"bestPrdList"}).find("h3").text)
-print(soup.find("div",{"id":"bestPrdList"}).find("div",{"class":"pname"}).p.text)
-
-
-cate = soup.find("div",{"id":"bestprdList"}).find("ul",{"class":"cfix"})
-lists=cate.find_all("li")
-
-print("1-28위 갯수: ",len(lists))
-
+# 1-28위 제목,가격 출력
+cate = soup.find("div",{"id":"bestPrdList"}).find("ul",{"class":"cfix"})
+lists = cate.find_all("li")
+print("1-28위 개수 :", len(lists))
 for idx,list in enumerate(lists):
-  p_title= list.find("div",{"class":"pname"}).p.text
-  p_price= list.find("strong",{"class":"sale_price"}).text
-  print(f"{idx+1}. {p_title}, 금액: {p_price} 원")
+  p_title = list.find("div",{"class":"pname"}).p.text
+  p_price = list.find("strong",{"class":"sale_price"}).text
+  print(f"{idx+1}. {p_title}, 금액 :{p_price} 원")
+
+# 제목 출력
+# print(soup.find("div",{"id":"bestPrdList"}).find("div",{"class":"pname"}).p.text)
